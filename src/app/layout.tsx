861df7349769fa18cfe62e0ac069b2362f767155
emirmehmedovic <emir.m@live.com>
2025-12-14 14:26:17 +0100

    Add Meta Pixel snippet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { Toaster } from 'react-hot-toast';
 import { ConditionalFooter } from '@/components/ConditionalFooter';
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://tpomerbasic.ba';
+const fbPixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID || '4243537145934095';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -79,6 +80,33 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-app min-h-screen`}
       >
+        {fbPixelId && (
+          <>
+            <Script id="fb-pixel" strategy="afterInteractive">
+              {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${fbPixelId}');
+                fbq('track', 'PageView');
+              `}
+            </Script>
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                src={`https://www.facebook.com/tr?id=${fbPixelId}&ev=PageView&noscript=1`}
+                alt="facebook pixel"
+              />
+            </noscript>
+          </>
+        )}
         {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
           <>
             <Script
